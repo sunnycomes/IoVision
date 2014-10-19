2ea5c200a596d4d410411ac864e661e2720a5e81
sunnycomes <sunnycomes@gmail.com>
2014-10-19 11:34:41 +0800

    Add function get_3rd_party_snippet in module settings.

--- src/common/settings.py
+++ src/common/settings.py
@@ -3,7 +3,7 @@ Created on Oct 6, 2014
 
 @author: sunnycomes
 '''
-
+import os
 from tornado.options import options
 
 def get_site_info():
@@ -16,3 +16,21 @@ def get_site_info():
     params["google_analytics_id"] = options.google_analytics_id
     
     return params
+
+def get_3rd_party_snippet():
+    snippets = {}
+    
+    dest_dir = options.global_resource_dir + os.sep + "snippet"
+    disqus_file = file(dest_dir + os.sep + "disqus.snippet")
+    disqus_snippet = disqus_file.read()
+    snippets["disqus_snippet"] = disqus_snippet.replace("param_disqus_shortname", options.disqus_shortname)
+    
+    google_analytics_file = file(dest_dir + os.sep + "google_analytics.snippet")
+    google_analytics_snippet = google_analytics_file.read()
+    snippets["google_analytics_snippet"] = google_analytics_snippet.replace("param_google_analytics_id", options.google_analytics_id)
+    
+    github_fork_file = file(dest_dir + os.sep + "github_fork.snippet")
+    github_fork_snippet = github_fork_file.read()
+    snippets["github_fork_snippet"] = github_fork_snippet.replace("param_github_link", options.github_link)
+    
+    return snippets

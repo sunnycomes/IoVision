a3e7832bb1262e8f470ee81a2741603dbfd6f58b
suncaijun <sun.caijun@bupt.edu.cn>
2016-04-15 14:03:42 +0800

    read keywords and description of the site from setup.cfg

--- src/common/settings.py
+++ src/common/settings.py
@@ -6,32 +6,33 @@ Created on Oct 6, 2014
 import os
 from tornado.options import options
 
-
 def get_site_info():
     params = {}
     params["author"] = options.author
     params["url"] = options.url
     params["title"] = options.title
+    params["keywords"] = options.keywords
+    params["description"] = options.description
     params["github_fork_link"] = options.github_fork_link
     params["disqus_shortname"] = options.disqus_shortname
     params["google_analytics_id"] = options.google_analytics_id
-    
+
     return params
 
 def get_3rd_party_snippet():
     snippets = {}
-    
+
     dest_dir = options.global_resource_dir + os.sep + "snippet"
     disqus_file = file(dest_dir + os.sep + "disqus.snippet")
     disqus_snippet = disqus_file.read()
     snippets["disqus_snippet"] = disqus_snippet.replace("param_disqus_shortname", options.disqus_shortname)
-    
+
     google_analytics_file = file(dest_dir + os.sep + "google_analytics.snippet")
     google_analytics_snippet = google_analytics_file.read()
     snippets["google_analytics_snippet"] = google_analytics_snippet.replace("param_google_analytics_id", options.google_analytics_id)
-    
+
     github_fork_file = file(dest_dir + os.sep + "github_fork.snippet")
     github_fork_snippet = github_fork_file.read()
     snippets["github_fork_snippet"] = github_fork_snippet.replace("param_github_fork_link", options.github_fork_link)
-    
+
     return snippets
--- src/common/utils.py
+++ src/common/utils.py
@@ -7,7 +7,6 @@ import ConfigParser
 import os, sys
 from tornado.options import define, options
 
-
 def set_author(author):
     define("author", default=author, help="Author of the project")
 
@@ -16,7 +15,13 @@ def set_url(url):
 
 def set_title(title):
     define("title", default=title, help="Site's title")
- 
+
+def set_keywords(keywords):
+    define("keywords", default=keywords, help="Site's keywords")
+
+def set_description(description):
+    define("description", default=description, help="Site's description")
+
 def set_github_fork_link(link):
     define("github_fork_link", default=link, help="Github source code link")
 
@@ -25,48 +30,50 @@ def set_disqus_shortname(name):
 
 def set_google_analytics_id(idx):
     define("google_analytics_id", default=idx, help="Google analytics id used to identify this site.")
-    
+
 def set_port(port):
     define("port", default=port, help="the port tornado listen to")
-    
+
 def set_source_dir(dirx):
     define("source_dir", default=os.getcwd() + os.sep + dirx, help="Source files directory")
-    
+
 def set_build_dir(dirx):
     define("build_dir", default=os.getcwd() + os.sep + dirx, help="Built files directory")
-    
+
 def set_post_dir(dirx):
     define("posts_dir", default=options.source_dir + os.sep + dirx, help="Post files directory")
 
 def set_about_dir(dirx):
     define("about_dir", default=options.source_dir + os.sep + dirx, help="About files directory")
-       
+
 def set_templates_dir(dirx):
     define("templates_dir", default=options.source_dir + os.sep + dirx, help="Templates directory, maybe not only one")
 
 def set_current_template_dir(name):
     define("current_template_dir", default=options.templates_dir + os.sep + name, help="Current template path")
 
-def set_static_resource_dir(dirx):    
+def set_static_resource_dir(dirx):
     define("static_resource_dir", default=options.current_template_dir + os.sep + dirx, help="Static resource directory")
 
-def set_global_resource_dir(dirx):    
+def set_global_resource_dir(dirx):
     define("global_resource_dir", default=options.source_dir + os.sep + dirx, help="Global resource directory")
-    
+
 def set_root_path(root_path):
     define("root_path", default=root_path, help="Project root path")
 
-def get_config_file(file_path):     
+def get_config_file(file_path):
     config = ConfigParser.RawConfigParser()
     config.read(file_path)
     return config
 
 def load_config(file_path):
     config = get_config_file(file_path)
-    
+
     set_author(config.get("sect_basic", "author"))
     set_url(config.get("sect_basic", "url"))
     set_title(config.get("sect_basic", "title"))
+    set_keywords(config.get("sect_basic",  "keywords"))
+    set_description(config.get("sect_basic", "description"))
     set_github_fork_link(config.get("sect_basic", "github_fork_link"))
     set_disqus_shortname(config.get("sect_basic", "disqus_shortname"))
     set_google_analytics_id(config.get("sect_basic", "google_analytics_id"))
@@ -79,10 +86,10 @@ def load_config(file_path):
     set_current_template_dir(config.get("sect_basic", "template_name"))
     set_static_resource_dir(config.get("sect_dir_tree", "static_resource_dir"))
     set_global_resource_dir(config.get("sect_dir_tree", "global_resource_dir"))
-       
+
 def init_root_path(root_path):
     set_root_path(root_path)
-	
+
     # change current working directory to root_path for relative paths to work correctly
     os.chdir(root_path)
     if os.access(root_path, os.F_OK):

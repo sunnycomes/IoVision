6b06aca79a4e1a398016a0f8cf0f063ee0427244
suncaijun <sun.caijun@bupt.edu.cn>
2016-04-23 20:16:28 +0800

    show the pager bar if article count greater than one page can holds

--- src/templates/default/static/js/script.js
+++ src/templates/default/static/js/script.js
@@ -4,31 +4,30 @@ $(function () {
 		function () {
 			$('html,body').animate({ scrollTop: 0 }, 700);
 	});
-	
+
 	$(".bottom").click(
 		function () {
             $('html,body').animate({ scrollTop: document.body.clientHeight }, 700);
 	});
-	
+
 })
 
 $(document).ready(function() {
 
 	var currentPage = 0;
 	var articleNumEachPage = 5;
-	
+
 	$("#blog_index article:gt(" + (articleNumEachPage - 1) + ")").hide();
 	$("#prev_page").hide();
 
-	
 	var articleNum = $("#blog_index article").length;
-	if(articleNum <= articleNumEachPage) {
-		$("#pager_bar").hide();
+	if(articleNum >= articleNumEachPage) {
+		$("#pager_bar").show();
 	}
-		
+
 	$("#next_page").click(function() {
 		$("#blog_index article").hide();
-		
+
 		currentPage = currentPage + 1;
 		var startIndex = currentPage * articleNumEachPage;
 		var endIndex = (currentPage + 1) * articleNumEachPage - 1;
@@ -38,7 +37,7 @@ $(document).ready(function() {
 				$(this).show();
 			}
 		});
-		
+
 		if(endIndex + 1 >= articleNum) {
 			$("#next_page").hide();
 		}
@@ -46,11 +45,11 @@ $(document).ready(function() {
 			$("#prev_page").show();
 		}
 	});
-	
+
 	$("#prev_page").click(function() {
 		$("#next_page").show();
 		$("#blog_index article").hide();
-		
+
 		currentPage = currentPage - 1;
 		var startIndex = currentPage * articleNumEachPage;
 		var endIndex = (currentPage + 1) * articleNumEachPage - 1;
@@ -60,7 +59,7 @@ $(document).ready(function() {
 				$(this).show();
 			}
 		});
-		
+
 		if(currentPage == 0) {
 			$("#prev_page").hide();
 			return;
